1c1b50311743015073e33207a45d44ae0e9fd1f7
Alex Cesca <alexcesca@gmail.com>
2022-01-17 15:54:32 -0300

    lopp e promriro get.

--- hello.go
+++ hello.go
@@ -2,29 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	exibeintroducao()
-	exibeMenu()
+	for {
+		exibeMenu()
 
-	comando, err := leComando()
-	if err != nil {
-		fmt.Println("Opção invalida ", comando)
-	}
+		comando, err := leComando()
+		if err != nil {
+			fmt.Println("Opção invalida ", comando)
+		}
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando ...")
-	case 2:
-		fmt.Println("Exibindo logs..")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Comando não conhecido")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs..")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando não conhecido")
+			os.Exit(-1)
+		}
 	}
 }
 
@@ -51,4 +54,17 @@ func exibeMenu() {
 	fmt.Println("1 - Iniciar monitoramento")
 	fmt.Println("2 - Exibir logs")
 	fmt.Println("0 - Sair do programa")
+}
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando ...")
+	site := "https://www.alura.com.br"
+	retorno, err := http.Get(site)
+	if err != nil {
+		fmt.Printf("Site %v OffLine. Status code %v", site, retorno.Status)
+	}
+	if retorno.StatusCode == 200 {
+		fmt.Printf("Site %v Online ", site)
+	} else {
+		fmt.Printf("Site %v OffLine. Status code %v", site, retorno.Status)
+	}
 }
